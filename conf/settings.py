b6ce28a8c60b48c40f219cf5984c73c47f0a4c2c
meritus freedy <meritusfreedy@gmail.com>
2022-01-19 21:37:53 -0500

    setup allowed host

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!moeww*ib9cg#ye)-2tbw76*fgqjp*igj6chvr%3)-djuzt2dr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
